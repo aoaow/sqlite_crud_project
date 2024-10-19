98d0979e6aff2de1c7992e286e7e61319d0674dc
aoaow <76165158+aoaow@users.noreply.github.com>
2024-10-18 20:05:06 -0400

    1

--- complex_sql_query.py
+++ complex_sql_query.py
@@ -16,7 +16,7 @@ def complex_query(conn):
                    (3, Cindy, 1, 66000),
                    (4, David, 3, 100000),
                    (5, Eleanor, 3, 240000),
-                   (6, Frya, 2, 120000);''')
+                   (6, Fryah, 2, 120000);''')
     # create table "department"
     # records the corresponding departments
     cursor.execute('''CREATE TABLE IF NOT EXISTS departments 
@@ -30,7 +30,7 @@ def complex_query(conn):
     cursor.execute('''SELECT d.name AS dpt_name, SUM(e.salary) AS total_salary
                    FROM employees e LEFT JOIN departments d ON e.dpt_id=d.id
                    GROUP BY d.id
-                   ORDER BY e.salary desc''')
+                   ORDER BY e.salary DESC''')
     # we left join employees and departments to get the department name
     # and then we group by department id so that we could get the department total salaries
     # rank the total salaries by deparment descendingly
